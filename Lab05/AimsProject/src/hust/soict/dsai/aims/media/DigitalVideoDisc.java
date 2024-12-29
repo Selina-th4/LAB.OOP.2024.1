4cd9a73e3d1b02155822ae5ff0cf7bf2e054c9f4
Selina-th4 <103742905+Selina-th4@users.noreply.github.com>
2024-12-29 22:48:12 +0700

    Update DigitalVideoDisc.java

--- Lab05/AimsProject/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
+++ Lab05/AimsProject/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
@@ -1,109 +1,75 @@
 package hust.soict.dsai.aims.media;
 
-public class DigitalVideoDisc extends Disc implements Playable {
-    private String director;
-    private int length;
-    private static int nbDigitalVideoDiscs = 0; // Class attribute for tracking the number of DVDs
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
-    public DigitalVideoDisc() {
-        super();
-        setId(++nbDigitalVideoDiscs); // Update the nbDigitalVideoDiscs and assign the id
-    }
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-    public DigitalVideoDisc(String title) {
-        super(title);
-        setId(++nbDigitalVideoDiscs); // Update the nbDigitalVideoDiscs and assign the id
-    }
-
-    public DigitalVideoDisc(String title, String category, float cost) {
-        super(title, category, cost);
-        setId(++nbDigitalVideoDiscs); // Update the nbDigitalVideoDiscs and assign the id
-    }
-
-    public DigitalVideoDisc(String title, String category, String director, float cost) {
-        super(title, category, director, cost);
-        this.director = director;
-        setId(++nbDigitalVideoDiscs); // Update the nbDigitalVideoDiscs and assign the id
-    }
-
-    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
-        super(title, category, director, length, cost);
-        this.director = director;
-        this.length = length;
-        setId(++nbDigitalVideoDiscs); // Update the nbDigitalVideoDiscs and assign the id
-    }
-
-    public String getDirector() {
-        return director;
-    }
-
-    public void setDirector(String director) {
-        this.director = director;
-    }
-
-    public int getLength() {
-        return length;
-    }
-
-    public void setLength(int length) {
-        this.length = length;
-    }
-
-    public String getFormattedLength() {
-        int hours = length / 3600;
-        int minutes = (length % 3600) / 60;
-        int seconds = length % 60;
-
-        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
-    }
-
-    public static int getNumberOfDVDs() {
-        return nbDigitalVideoDiscs;
-    }
-
-    @Override
-    public void play() {
-        System.out.println("Playing DVD: " + getTitle());
-        System.out.println("DVD length: " + getFormattedLength());
-    }
-
-    
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Title: ").append(getTitle()).append("\n");
-        sb.append("Category: ").append(getCategory()).append("\n");
-        sb.append("Director: ").append(getDirector()).append("\n");
-        sb.append("Length: ").append(getFormattedLength()).append("\n");
-        sb.append("Price: ").append(getCost()).append(" $").append("\n");
-        
-        sb.append("\n");
-
-        return sb.toString();
-    }
+import hust.soict.dsai.aims.exception.*;
 
 
-	@Override
-	public void stop() {
-		// TODO Auto-generated method stub
-		
+public class DigitalVideoDisc extends Disc {
+	
+	public String getType() {
+		return "DVD";
 	}
-
-	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-		
+	
+	public String getDetails() {
+		return ("Product ID: " + String.valueOf(this.getID())
+				+ "\n" + "\t" + "Title: " + this.getTitle()
+				+ "\n" + "\t" + "Category: " + this.getCategory()
+				+ "\n" + "\t" + "Director: " + this.getDirector()
+				+ "\n" + "\t" + "Length: " + String.valueOf(this.getLength()) + " minutes"
+				+ "\n" + "\t" + "Price: $" + String.valueOf(this.getCost()));
 	}
-
-	@Override
-	public void resume() {
-		// TODO Auto-generated method stub
-		
+	
+	public void play() throws PlayerException {
+		if (this.getLength() <= 0) {
+			throw new PlayerException("ERROR: DVD length is non-positive!");
+		} else {
+			System.out.println("Playing DVD: " + this.getTitle());
+			System.out.println("DVD length: " + this.getLength());
+			
+			Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+			JPanel p = new JPanel();
+			JDialog d = new JDialog();
+			JLabel l1 = new JLabel("Now playing: " + this.getTitle());
+			JLabel l2 = new JLabel("DVD length: " + this.getLength());
+			p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+			l1.setAlignmentX(Component.CENTER_ALIGNMENT);
+			l2.setAlignmentX(Component.CENTER_ALIGNMENT);
+			d.setTitle("Media Player");
+			p.add(Box.createVerticalGlue());
+			p.add(l1);
+			p.add(l2);
+			p.add(Box.createVerticalGlue());
+			d.add(p);
+			d.setSize(250,100);
+			int w = d.getSize().width;
+	        int h = d.getSize().height;
+	        int x = (dim.width - w) / 2;
+	        int y = (dim.height - h) / 2;
+			d.setLocation(x, y);
+			d.setVisible(true);
+		}
 	}
-
-	@Override
-	public boolean isPlaying() {
-		// TODO Auto-generated method stub
-		return false;
+	
+	public DigitalVideoDisc(String title) {
+		super(title);
 	}
+	public DigitalVideoDisc(String title, String category, float cost) {
+		super(title, category, cost);
+	}
+	public DigitalVideoDisc(String title, String category, String director, float cost) {
+		super(title, category, director, cost);
+	}
+	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+		super(title, category, director, length, cost);
+	}
+	
 }

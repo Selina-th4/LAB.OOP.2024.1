a3da366ee88c87bee6dc687b948541443a187b23
Selina-th4 <103742905+Selina-th4@users.noreply.github.com>
2024-12-29 22:48:19 +0700

    Update MediaComparatorByCostTitle.java

--- Lab05/AimsProject/src/hust/soict/dsai/aims/media/MediaComparatorByCostTitle.java
+++ Lab05/AimsProject/src/hust/soict/dsai/aims/media/MediaComparatorByCostTitle.java
@@ -1,23 +1,15 @@
 package hust.soict.dsai.aims.media;
-
 import java.util.Comparator;
 
-
 public class MediaComparatorByCostTitle implements Comparator<Media> {
-    @Override
-    public int compare(Media media1, Media media2) {
-        if (media1 == null && media2 == null) {
-            return 0;
-        } else if (media1 == null) {
-            return -1;
-        } else if (media2 == null) {
-            return 1;
-        }
-        
-        int result = Float.compare(media1.getCost(), media2.getCost());
-        if (result == 0) {
-            result = media1.getTitle().compareTo(media2.getTitle());
-        }
-        return result;
-    }
+
+	public int compare(Media m1, Media m2) {
+		if (m1.getCost() < m2.getCost()) {
+			return 1;
+		} else if (m1.getCost() > m2.getCost()) {
+			return -1;
+		} else {
+			return m1.getTitle().compareToIgnoreCase(m2.getTitle());
+		}
+	}
 }

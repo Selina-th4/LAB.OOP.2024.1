50d76bd0a1119386c97158ef7062a36117c9dd5f
Selina-th4 <103742905+Selina-th4@users.noreply.github.com>
2024-12-29 22:48:08 +0700

    Update Book.java

--- Lab05/AimsProject/src/hust/soict/dsai/aims/media/Book.java
+++ Lab05/AimsProject/src/hust/soict/dsai/aims/media/Book.java
@@ -1,102 +1,77 @@
 package hust.soict.dsai.aims.media;
-
 import java.util.ArrayList;
 import java.util.List;
 
+import hust.soict.dsai.aims.exception.*;
+
 public class Book extends Media {
-    private List<String> authors = new ArrayList<>();
-    private int numberOfPages;
-
-    public Book() {
-        super();
-    }
-
-    public Book(String title, String category, float cost, int numberOfPages) {
-        super(title, category, cost);
-        this.numberOfPages = numberOfPages;
-    }
-
-    // Accessor methods for authors field
-    public List<String> getAuthors() {
-        return authors;
-    }
-
-    // Add an author to the authors list
-    public void addAuthor(String authorName) {
-        if (isValidAuthor(authorName)) {
-            if (!authors.contains(authorName)) {
-                authors.add(authorName);
-            }
-        } else {
-            System.out.println("Invalid author name.");
-        }
-    }
-
-    // Remove an author from the authors list
-    public void removeAuthor(String authorName) {
-        if (isValidAuthor(authorName)) {
-            authors.remove(authorName);
-        } else {
-            System.out.println("Invalid author name.");
-        }
-    }
-
-    private boolean isValidAuthor(String authorName) {
-        return !authorName.isEmpty();
-    }
-
-    private String authorsToString() {
-        StringBuilder sb = new StringBuilder();
-        for (String author : authors) {
-            sb.append(author).append(", ");
-        }
-        if (sb.length() > 2) {
-            sb.setLength(sb.length() - 2);
-        }
-        return sb.toString();
-    }
-    public void displayInfo() {
-            System.out.println("Title: " + getTitle());
-            System.out.println("Category: " + getCategory());
-            System.out.println("Cost: " + getCost());
-            System.out.println("Authors: " + authorsToString());
-            System.out.println("Number of Pages: " + numberOfPages);
-            System.out.println();
-        }
-    
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Title: ").append(getTitle()).append("\n");
-        sb.append("Category: ").append(getCategory()).append("\n");
-        sb.append("Cost: ").append(getCost()).append("\n");
-        sb.append("Authors: ").append(authorsToString()).append("\n");
-        sb.append("Number of Pages: ").append(numberOfPages);
-       
-        sb.append("\n");
-
-        return sb.toString();
-    }
-
-    public String getType() {
-        return "Book";
-    }
-
-    public String getDirector() {
-        return "";
-    }
-
-    public boolean isMatch(String title) {
-        return getTitle().equalsIgnoreCase(title);
-    }
-
-    public boolean filterByCategory(String category) {
-        return getCategory().equalsIgnoreCase(category);
-    }
-
-	public Object addAuthor() {
-		// TODO Auto-generated method stub
-		return null;
+	private int contentLength;
+	private List<String> authors = new ArrayList<String>();
+	
+	public void addAuthor(String authorName) throws DupplicatedItemException {
+		for (String name: this.authors) {
+			if (name.toLowerCase().equals(authorName.toLowerCase())) {
+				throw new DupplicatedItemException(name + " is already in the list of authors.");
+			}
+		}
+		this.authors.add(authorName);
+		System.out.println(authorName + " has been added to the " + this.getTitle() + " list of authors.");
 	}
+	
+	
+	public void removeAuthor(String authorName) throws NonExistingItemException {
+		for (String name: this.authors) {
+			if (name.toLowerCase().equals(authorName.toLowerCase())) {
+				this.authors.remove(name);
+				System.out.println(name + " has been removed from the " + this.getTitle() + " list of authors.");
+				return;
+			}
+		}
+		throw new NonExistingItemException(authorName + " is not in the list of authors.");
+	}
+	
+	public String getType() {
+		return "Book";
+	}
+	
+	public String getDetails() {
+		StringBuffer authorsList = new StringBuffer();
+		if (this.authors.size() >= 1) {
+			authorsList.append(this.authors.get(0));
+			for (int i = 1; i < this.authors.size(); i++) {
+				authorsList.append(", " + this.authors.get(i));
+			}
+		}
+		return ("Product ID: " + String.valueOf(this.getID())
+		+ "\n" + "\t" + "Title: " + this.getTitle()
+		+ "\n" + "\t" + "Category: " + this.getCategory()
+		+ "\n" + "\t" + "Authors: " + authorsList
+		+ "\n" + "\t" + "Content Length: " + String.valueOf(this.getContentLength()) + " pages"
+		+ "\n" + "\t" + "Price: $" + String.valueOf(this.getCost()));
+	}
+	
+	public Book(String title, String category, int contentLength, float cost) {
+		super(title, category, cost);
+		this.contentLength = contentLength;
+	}
+
+	public Book(String title, String category, float cost) {
+		super(title, category, cost);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Book(String title, String category) {
+		super(title, category);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Book(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getContentLength() {
+		return contentLength;
+	}
+
 }
